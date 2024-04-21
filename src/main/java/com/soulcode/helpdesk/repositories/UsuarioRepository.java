46d1926bfc4518904131afdd84f45f86b18a0e26
slrocha <slrocha17@gmail.com>
2024-04-21 11:29:43 -0300

    Adicionando o esqueleto do css nas páginas da aplicação e add comentário no properties para informações relacionadas ao banco de dados

--- src/main/java/com/soulcode/helpdesk/repositories/UsuarioRepository.java
+++ src/main/java/com/soulcode/helpdesk/repositories/UsuarioRepository.java
@@ -4,7 +4,6 @@ import com.soulcode.helpdesk.models.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
     List<UsuarioModel> findByLogin(String login);
